ac033491bc03be7d0a8d5659eb26b0d83e610727
AmulyaParitosh <amulya1171@gmail.com>
2021-12-27 16:08:14 +0530

    cleaning main

--- main.py
+++ main.py
@@ -88,65 +88,63 @@ def predict_dis():
     precau.config(state='disabled')
 
 
-root = Tk()
-
-content = ttk.Frame(root, padding=(10, 10, 10, 10))
-frame = Frame(content, borderwidth=4, relief="ridge", width=1710, height=955)
-
-button_img = PhotoImage(file="button_img.png")
-
-title = Label(content, text="Disease Pridiction Bot",
-              width=188, height=3, background="grey")
-
-about = Label(content, text="This is about our project",
-              width=95, height=10)
-
-speak = Button(content, text="press to speak",
-               width=45, height=19)
-
-add_sympt = Button(content, text="Add Symptom",
-                   command=add_symptom, width=43, height=3)
-
-var2 = StringVar()
-var2.set(sympts)
-sympt_list = Listbox(content, listvariable=var2, width=46, height=15)
-
-txt = Text(content, width=95, height=7)
-
-clear = Button(content, text="clear", command=clear_txt, width=92)
-
-do_predict = Button(content, text="Image", image=button_img,
-                    command=predict_dis, width=915, height=230)
-
-dis_name = Text(content, width=91, height=3)
-dis_name.insert("1.0", "Disease Name")
-dis_name.config(state='disabled')
-
-about_dis = Text(content, width=45, height=25)
-about_dis.insert("1.0", "About Disease")
-about_dis.config(state='disabled')
-
-precau = Text(content, width=45, height=25)
-precau.insert("1.0", "Precaution of Disease")
-precau.config(state='disabled')
-
-
-title.grid(column=0, row=0, columnspan=4)
-about.grid(column=0, row=1, columnspan=2)
-speak.grid(column=0, row=2, rowspan=2)
-add_sympt.grid(column=1, row=2)
-sympt_list.grid(column=1, row=3)
-txt.grid(column=0, row=4, columnspan=2)
-clear.grid(column=0, row=5, columnspan=2)
-do_predict.grid(column=2, row=1, columnspan=2)
-dis_name.grid(column=2, row=2, columnspan=2)
-about_dis.grid(column=2, row=3, rowspan=3)
-precau.grid(column=3, row=3, rowspan=3)
-
-content.grid(column=0, row=0)
-
-root.mainloop()
-
-
 if __name__ == "__main__":
-    pass
+
+    root = Tk()
+
+    content = ttk.Frame(root, padding=(10, 10, 10, 10))
+    frame = Frame(content, borderwidth=4,
+                  relief="ridge", width=1710, height=955)
+
+    button_img = PhotoImage(file="button_img.png")
+
+    title = Label(content, text="Disease Pridiction Bot",
+                  width=188, height=3, background="grey")
+
+    about = Label(content, text="This is about our project",
+                  width=95, height=10)
+
+    speak = Button(content, text="press to speak",
+                   width=45, height=19)
+
+    add_sympt = Button(content, text="Add Symptom",
+                       command=add_symptom, width=43, height=3)
+
+    var2 = StringVar()
+    var2.set(sympts)
+    sympt_list = Listbox(content, listvariable=var2, width=46, height=15)
+
+    txt = Text(content, width=95, height=7)
+
+    clear = Button(content, text="clear", command=clear_txt, width=92)
+
+    do_predict = Button(content, text="Image", image=button_img,
+                        command=predict_dis, width=915, height=230)
+
+    dis_name = Text(content, width=91, height=3)
+    dis_name.insert("1.0", "Disease Name")
+    dis_name.config(state='disabled')
+
+    about_dis = Text(content, width=45, height=25)
+    about_dis.insert("1.0", "About Disease")
+    about_dis.config(state='disabled')
+
+    precau = Text(content, width=45, height=25)
+    precau.insert("1.0", "Precaution of Disease")
+    precau.config(state='disabled')
+
+    title.grid(column=0, row=0, columnspan=4)
+    about.grid(column=0, row=1, columnspan=2)
+    speak.grid(column=0, row=2, rowspan=2)
+    add_sympt.grid(column=1, row=2)
+    sympt_list.grid(column=1, row=3)
+    txt.grid(column=0, row=4, columnspan=2)
+    clear.grid(column=0, row=5, columnspan=2)
+    do_predict.grid(column=2, row=1, columnspan=2)
+    dis_name.grid(column=2, row=2, columnspan=2)
+    about_dis.grid(column=2, row=3, rowspan=3)
+    precau.grid(column=3, row=3, rowspan=3)
+
+    content.grid(column=0, row=0)
+
+    root.mainloop()
